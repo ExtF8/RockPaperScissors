cacb91caa17efc1a7e3b9b71b126a23cec6a5fd7
extf8 <extfield8@gmail.com>
2022-07-09 12:14:43 +0200

    Refactored playRound

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 
 let computerScore = 0;
 let playerScore = 0;
-// const buttons = document.querySelectorAll('button');
-let playerSelection;
 let scoreCount = 0;
 
-
+const userScore = document.querySelector('.user-score');
+const compScore = document.querySelector('.computer-score');
 
 const rockBtn = document.querySelector('#rock');
 const paperBtn = document.querySelector('#paper');
 const scissorsBtn = document.querySelector('#scissors');
 
-// const buttons = document.querySelectorAll('button');
+
 
 
 
@@ -25,7 +24,7 @@ function computerPlay() {
 rockBtn.addEventListener('click', () => {
 	playerSelection = 'rock';
 	computerPlay();
-	game();
+	playRound();
 });
 
 paperBtn.addEventListener('click', () => {
@@ -43,72 +42,30 @@ scissorsBtn.addEventListener('click', () => {
 function playRound(playerSelection) {
     let player = playerSelection;
     let computer = computerPlay();
-
-    if (player == 'rock' && computer == 'scissors') {
+	
+    if (
+		player == 'rock' && computer == 'scissors' ||
+		player == 'paper' && computer == 'rock' || 
+		player == 'scissors' && computer == 'paper'
+	) {
         playerScore++;
+		userScore.textContent = `${playerScore}`;
         return 'Win';
 
     } 
-	else if (player == 'rock' && computer == 'paper') {
+	else if (
+		player == 'rock' && computer == 'paper' ||
+		player == 'paper' && computer == 'scissors' ||
+		player == 'scissors' && computer == 'rock'
+	) {
         computerScore++;
+		compScore.textContent = `${computerScore}`;
         return 'Lose';
-
     } 
-	else if (player == 'paper' && computer == 'scissors') {
-        computerScore++;
-        return 'Lose';
-
-    } 
-	else if (player == 'paper' && computer == 'rock') {
-        playerScore++;
-        return 'Win';
-
-    } 
-	else if (player == 'scissors' && computer == 'rock') {
-        computerScore++;
-        return 'Lose';
-
-    } 
-	else if (player == 'scissors' && computer == 'paper') {
-        playerScore++;
-        return 'Win';
-    }
-    else 
+	else 
 	return 'Draw';
 };
 
-
-function game() {
-		let computerSelection = computerPlay();
-		console.log(playRound(playerSelection, computerSelection));
-
-		if (playerScore > computerScore) {
-			console.log(`You win!`);
-		} 
-		else if (computerScore > playerScore) {
-			console.log(`You suck!`);
-		} 
-		else if (playerScore = computerScore) {
-			console.log(`You tied.`);
-		}
-		else if (playerScore === 5 ) {
-			console.log("Win!");
-		}
-		else (computerScore === 5) 
-			console.log("You suck!");
-		
-	}
-	
-// game();
-				
-// function game() {
-// 	if (playerScore === 5) {
-// 		console.log("Win!");
-// 	}
-// 	else if (computerScore === 5) {
-// 		console.log("You suck!");
-// 	}
-// }
 
 function game() {
 	let computerSelection = computerPlay();
