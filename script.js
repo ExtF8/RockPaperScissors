6189dc7b3382b18161944f23ff1b2d57ab8f7564
extf8 <extfield8@gmail.com>
2022-07-23 21:53:11 +0200

    Fixed eventListener and script import

--- script.js
+++ script.js
@@ -1,90 +1,94 @@
-
-let computerScore = 0;
-let playerScore = 0;
-let scoreCount = 0;
-
-const userScore = document.querySelector('.user-score');
-const compScore = document.querySelector('.computer-score');
-
-const rockBtn = document.querySelector('#rock');
-const paperBtn = document.querySelector('#paper');
-const scissorsBtn = document.querySelector('#scissors');
-
-
-
-
-
-
-function computerPlay() {
-    let items = ['rock', 'paper', 'scissors'];
-    const randomChoice = Math.floor(Math.random() * items.length);
-    return items[randomChoice];
-};
-
-rockBtn.addEventListener('click', () => {
-	playerSelection = 'rock';
-	computerPlay();
-	playRound();
-});
-
-paperBtn.addEventListener('click', () => {
-	playerSelection = 'paper';
-	computerPlay();
-	game();
-});
-
-scissorsBtn.addEventListener('click', () => {
-	playerSelection = 'scissors';
-	computerPlay();
-	game();
-});
-
-function playRound(playerSelection) {
-    let player = playerSelection;
-    let computer = computerPlay();
+window.onload = () => {
+	let computerScore = 0;
+	let playerScore = 0;
+	// let scoreCount = 0;
+	let roundCount = 1;
 	
-    if (
-		player == 'rock' && computer == 'scissors' ||
-		player == 'paper' && computer == 'rock' || 
-		player == 'scissors' && computer == 'paper'
-	) {
-        playerScore++;
-		userScore.textContent = `${playerScore}`;
-        return 'Win';
-
-    } 
-	else if (
-		player == 'rock' && computer == 'paper' ||
-		player == 'paper' && computer == 'scissors' ||
-		player == 'scissors' && computer == 'rock'
-	) {
-        computerScore++;
-		compScore.textContent = `${computerScore}`;
-        return 'Lose';
-    } 
-	else 
-	return 'Draw';
-};
-
-
-function game() {
-	let computerSelection = computerPlay();
-	console.log(playRound(playerSelection, computerSelection));
-
-	if (scoreCount >= 4) {
-		if (computerScore > playerScore) {
-			console.log('You Lost this Game')
+	let userScore = document.querySelector('.user-score');
+	let compScore = document.querySelector('.computer-score');
+	
+	const roundNum = document.querySelector('.round-number');
+	
+	const rockBtn = document.querySelector('#rock');
+	const paperBtn = document.querySelector('#paper');
+	const scissorsBtn = document.querySelector('#scissors');
+	
+	const compRockBtn = document.querySelector('.computer-rock');
+	const compPaperBtn  = document.querySelector('.computer-paper');
+	const compScissorsBtn = document.querySelector('.computer-scissors');
+	
+	let result = document.querySelector('.result');
+	
+	rockBtn.addEventListener('click', () => {playRound('rock')});
+	paperBtn.addEventListener('click', () => {playRound('paper')});
+	scissorsBtn.addEventListener('click', () => {playRound('scissors')});
+	
+	
+	function computerPlay() {
+		const number = Math.floor(Math.random() * 999);
+		if (number % 3 === 0) {
+			return 'rock';
 		}
-		else if (computerScore < playerScore) {
-			console.log('You Won this Game')
+		if (number % 3 === 1) {
+			return 'paper';
 		}
+		return 'scissors'; 
+	}
+	
+	// function computerPlay() {
+	//     let items = ['rock', 'paper', 'scissors'];
+	//     const randomChoice = Math.floor(Math.random() * items.length);
+	//     return items[randomChoice];
+	// }
+	
+	
+	
+	
+	
+	function playRound(playerSelection) {
+		computer = computerPlay();
+		player = playerSelection;
+		
+		
+		if (
+			(player == 'rock' && computer == 'scissors') || 
+			(player == 'paper' && computer == 'rock')|| 
+			(player == 'scissors' && computer == 'paper')
+		) {
+			playerScore++;
+			userScore.textContent = `${playerScore}`;
+			result.textContent = `You won round #${roundCount}`;
+			if (playerScore == 5) {
+				result.textContent = `You WON! You got 5 points in ${roundCount} rounds`
+			}
+			roundCount++;
+			roundNum.textContent = `${roundCount}`;
+		} 
+		else if (player == computer) {
+			roundCount++;
+			roundNum.textContent = `${roundCount}`;
+			result.textContent = `This round is draw. You both choose same.`;
+		} 
 		else {
-			console.log('Tie');
+			computerScore++;
+			compScore.textContent = `${computerScore}`;
+			result.textContent = `You lost round #${roundCount}`;
+				if (computerScore == 5) {
+					result.textContent = `You suck! You lost to Computer in ${roundCount} rounds! Go and do something useful!`
+				}
+			roundCount++;
+			roundNum.textContent = `${roundCount}`;
 		}
-		// endGame()
 	}
-	else {
-		scoreCount++;
-	}
+}
 
-};
+
+
+
+
+
+
+
+
+
+
