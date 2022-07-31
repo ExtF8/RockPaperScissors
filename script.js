2c02878622042674bb7fac1d99d5dc9d314db8cd
extf8 <extfield8@gmail.com>
2022-07-31 13:01:36 +0200

    Added 5 point limit and refresh button
    Added animations to human and computer

--- script.js
+++ script.js
@@ -7,15 +7,82 @@ const userScore = document.querySelector('.user-score');
 const compScore = document.querySelector('.computer-score');
 const result = document.querySelector('.result');
 const roundNum = document.querySelector('.round-number');
-
+const resultContent = document.querySelector('.result-content');
 
 const btns = document.querySelectorAll('button[data-selection]');
+
+const rockButton = document.querySelector('.rock');
+const paperButton = document.querySelector('.paper');
+const scissorsButton = document.querySelector('.scissors');
+
+const computerRock = document.querySelector('.computer-rock');
+const computerPaper = document.querySelector('.computer-paper');
+const computerScissors = document.querySelector('.computer-scissors');
+
+rockButton.addEventListener('mouseover', userHover);
+paperButton.addEventListener('mouseover', userHover);
+scissorsButton.addEventListener('mouseover', userHover);
+
+
+rockButton.addEventListener('mouseleave', removeHover);
+paperButton.addEventListener('mouseleave', removeHover);
+scissorsButton.addEventListener('mouseleave', removeHover);
+
+const newGame = document.createElement('button');
+
+newGame.textContent = 'Play again!';
+newGame.classList.add('button', 'refresh');
+
+newGame.addEventListener('mouseover', refreshHover);
+newGame.addEventListener('mouseleave', removeHover);
+newGame.addEventListener('click', refreshHover);
+
+function refreshHover() {
+	newGame.classList.add('userHover');
+}
+
+function removeClick() {
+	this.classList.remove('userClick');
+	this.classList.remove('userHover');
+}
+
+function removeHover() {
+	this.classList.remove('userHover');
+}
+
+function refreshPage() {
+	window.location.reload(true);
+}
+
+function userHover() {
+	if (playerScore <= 4 && computerScore <= 4) {
+		this.classList.add('userHover');
+	}
+}
+
+
+function removeUserColor() {
+	rockButton.classList.remove('userClick');
+    paperButton.classList.remove('userClick');
+    scissorsButton.classList.remove('userClick');
+}
+
+function disableButtons() {
+    btns.forEach(elem => {
+        elem.disabled = true
+    })
+}
+
+newGame.addEventListener('click', refreshPage);
+
 [...btns].forEach(btn => {
 	let choice = btn.getAttribute('data-selection');
 	btn.addEventListener('click', () => {
-		playRound(choice)
+		playRound(choice);
+		btn.classList.add('userClick');
 	})
 })
+
 
 function computerPlay() {
 	let result = 'scissors';
@@ -29,11 +96,14 @@ function computerPlay() {
 	}
 
 	return result;
+	
 }
 
 function playRound(playerSelection) {
 	computer = computerPlay();
 	player = playerSelection;
+	removeUserColor();
+	computerColor(computer);
 
 	if (
 		(player == 'rock' && computer == 'scissors') ||
@@ -45,7 +115,9 @@ function playRound(playerSelection) {
 		result.textContent = `You won round #${roundCount}`;
 
 		if (playerScore == 5) {
-			result.textContent = `You WON! You got 5 points in ${roundCount} rounds`
+			result.textContent = `You WON! You got 5 points in ${roundCount} rounds!`;
+			resultContent.insertAdjacentElement('afterend', newGame);
+			disableButtons();
 		}
 		
 		roundCount++;
@@ -60,10 +132,40 @@ function playRound(playerSelection) {
 		result.textContent = `You lost round #${roundCount}`;
 
 		if (computerScore == 5) {
-			result.textContent = `You suck! You lost to Computer in ${roundCount} rounds! Go and do something useful!`
+			result.textContent = `You suck! You lost to Computer in ${roundCount} rounds! Go and do something useful!`;
+			resultContent.insertAdjacentElement('afterend', newGame);
+			disableButtons();
 		}
 
 		roundCount++;
 		roundNum.textContent = `${roundCount}`;
 	}
 }
+
+function computerColor(computer) {
+	if (computer === 'rock') {
+			removeColor();
+			computerRock.classList.add('computerPick');
+
+	} else if (computer === 'paper') {
+			removeColor();
+			computerPaper.classList.add('computerPick');
+	} else {
+			removeColor();
+			computerScissors.classList.add('computerPick');
+	}
+}
+
+function removeColor() {
+	computerRock.classList.remove('computerPick');
+	computerPaper.classList.remove('computerPick');
+	computerScissors.classList.remove('computerPick');
+}
+
+computerRock.addEventListener('transitionend', removeColor);
+computerPaper.addEventListener('transitionend', removeColor);
+computerScissors.addEventListener('transitionend', removeColor);
+
+rockButton.addEventListener('transitionend', removeClick);
+paperButton.addEventListener('transitionend', removeClick);
+scissorsButton.addEventListener('transitionend', removeClick);
